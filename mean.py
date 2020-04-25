cf6318c3271aef80e60de7b038e5dfb4ebd9b326
Harry H. Toigo <hhtoigo@gmail.com>
2020-04-24 18:13:09 -0700

    Added std() function and tests, using TDD.

--- mean.py
+++ mean.py
@@ -1,3 +1,4 @@
+from math import sqrt
 
 def mean(num_list):
     if len(num_list) > 0:
@@ -10,5 +11,19 @@ def mean(num_list):
     except TypeError as detail:
         msg = "The mean of a non-numeric list is undefined."
         raise TypeError(detail.__str__() + '\n' + msg)
-    except OverflowError as error:
+    except OverflowError:
+        return 0
+
+# Standard deviation
+
+def std(nums):
+    try:
+        if len(nums) == 0:
+            return 0.0
+        m = mean(nums)
+        return sqrt(sum((x - m) ** 2 for x in nums) / len(nums))
+    except TypeError as error:
+        msg = "The standard deviation os a non-numeric list is undefined."
+        raise TypeError(error.__str__() + '\n' + msg)
+    except OverflowError:
         return 0
--- test_mean.py
+++ test_mean.py
@@ -1,4 +1,7 @@
-from mean import mean
+from mean import mean, std
+from numpy.testing import assert_allclose
+
+# mean() tests
 
 def test_ints():
     num_list = [1, 2, 3, 4, 5]
@@ -30,4 +33,32 @@ def test_complex():
     num_list = [2 + 3j, 3 + 4j, -32 - 2j]
     obs = mean(num_list)
     exp = NotImplemented
+    assert obs == exp
+
+
+# std() tests
+
+def test_std1():
+    obs = std([0.0, 2.0])
+    exp = 1.0
+    assert obs == exp
+
+def test_std2():
+    obs = std([])
+    exp = 0.0
+    assert obs == exp
+
+def test_std3():
+    obs = std([5.0])
+    exp = 0.0
+    assert obs == exp
+
+def test_std4():
+    obs = std([1.0, 2.0, 3.0, 4.0, 5.0])
+    exp = 1.41421
+    assert_allclose(obs, exp, atol=0.00001, rtol=0)
+
+def test_std5():
+    obs = std([11.0, 11.0, 11.0, 11.0, 11.0, 11.0])
+    exp = 0.0
     assert obs == exp
